de4e56938f7595bef384ebbd84062cf5ed699b27
Alexey Kuleshov <kulesa@gmail.com>
2022-11-09 10:04:10 +0200

    messing around

--- nbdev_playground/_modidx.py
+++ nbdev_playground/_modidx.py
@@ -5,4 +5,5 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://kulesa.github.io',
                 'git_url': 'https://github.com/kulesa/nbdev_playground',
                 'lib_path': 'nbdev_playground'},
-  'syms': {'nbdev_playground.core': {'nbdev_playground.core.foo': ('core.html#foo', 'nbdev_playground/core.py')}}}
+  'syms': { 'nbdev_playground.core': { 'nbdev_playground.core.foo': ('core.html#foo', 'nbdev_playground/core.py'),
+                                       'nbdev_playground.core.say_hello': ('core.html#say_hello', 'nbdev_playground/core.py')}}}
